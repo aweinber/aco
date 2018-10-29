0bb84c11a6cda62ad90ea147b46a4346b3268740
sean cork <seantcork@gmaol.com>
2018-10-29 11:24:59 -0400

    Made function to read in cities

--- src/main/java/City.java
+++ src/main/java/City.java
@@ -1,2 +1,10 @@
 public class City {
+    private double xcord;
+    private double ycord;
+
+    public City(double xcord, double ycord) {
+        this.xcord = xcord;
+        this.ycord = ycord;
+    }
+
 }
--- src/main/java/Edge.java
+++ src/main/java/Edge.java
@@ -1,3 +1,42 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 public class Edge{
 
+    public static void main(){
+
+        ArrayList<City> cities= readCities();
+        for(int i = 0; i < cities.size(); i++){
+            System.out.print(cities.get(i));
+        }
+
+    }
+
+    private static ArrayList<City> readCities(String fileName){
+        BufferedReader reader;
+
+        ArrayList<City> cities = new ArrayList<City>();
+
+        try {
+            reader = new BufferedReader(new FileReader(fileName));
+            String line = reader.readLine();
+            while (line != null) {
+                System.out.println(line);
+                // read next line
+                line = reader.readLine();
+                string array1[]= line.split(" ");
+                double x_cord = string(array1[0]);
+                double y_cord = string (array1[1]);
+                City city = new City(x_cord, y_cord);
+                cities.add(city);
+
+
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return cities;
+    }
 }
