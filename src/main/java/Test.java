1b89ce0c5047976695456207382b34191202311d
sean cork <seantcork@gmaol.com>
2018-11-06 11:15:27 -0500

    Added a testing function to the test class.

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -32,24 +32,25 @@ public class Test{
             evaporation_factor = 0.1;
             elitism_factor = num_ants;
             epsilon = 0.1;
+            testing();
 
-            TSP problem = new TSP("eil51.tsp", num_ants ,426.0);
-
-            int termination_condition = 3;
-
-
-            EAS eas = new EAS(problem, num_ants, max_iterations, alpha, beta, evaporation_factor, elitism_factor, termination_condition);
-            eas.execute_eas();
-            System.out.println("EAS best: " + eas.get_best_length());
-
-
-            problem = new TSP("eil51.tsp", num_ants, 426.0);
-
-            System.out.println("ACO alogrithm");
-            ACO aco = new ACO(problem, num_ants, max_iterations, alpha, beta, evaporation_factor, epsilon, termination_condition);
-            aco.execute_aco();
-
-            System.out.println("Best length: " + aco.get_best_length());
+//            TSP problem = new TSP("eil51.tsp", num_ants ,426.0);
+//
+//            int termination_condition = 3;
+//
+//
+//            EAS eas = new EAS(problem, num_ants, max_iterations, alpha, beta, evaporation_factor, elitism_factor, termination_condition);
+//            eas.execute_eas();
+//            System.out.println("EAS best: " + eas.get_best_length());
+//
+//
+//            problem = new TSP("eil51.tsp", num_ants, 426.0);
+//
+//            System.out.println("ACO alogrithm");
+//            ACO aco = new ACO(problem, num_ants, max_iterations, alpha, beta, evaporation_factor, epsilon, termination_condition);
+//            aco.execute_aco();
+//
+//            System.out.println("Best length: " + aco.get_best_length());
 
 
         }
